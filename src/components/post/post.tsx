fb6be255771d1855a2e878260e5729a16294e83d
Piotr Kuncy <piotr.kuncy@neuca.pl>
2024-08-14 14:34:59 +0200

    feat: json-ld schema for post

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -4,17 +4,54 @@ import {
   TypographyP,
 } from "@/components/ui/typography";
 import type { RouterOutputs } from "@/trpc/react";
+import type { BlogPosting, WithContext } from "schema-dts";
 
 export default function Post({
   data,
 }: {
   data: Exclude<RouterOutputs["post"]["get"], null>;
 }) {
+  const jsonLd: WithContext<BlogPosting> = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    headline: data.title,
+    alternativeHeadline:
+      data.title.length > 24
+        ? data.title.split(" ").slice(0, 5).join(" ")
+        : data.title,
+    editor: data.createdBy.name,
+    wordCount: data.content.split(" ").length,
+    datePublished: data.createdAt.toISOString(),
+    dateModified: data.updatedAt?.toISOString(),
+    description: "Blog post on ABC Blog",
+    articleBody: data.content,
+    author: {
+      "@type": "Person",
+      name: data.createdBy.name,
+    },
+    creator: {
+      "@type": "Person",
+      name: data.createdBy.name,
+    },
+    inLanguage: "en-US",
+    genre: "blog",
+    articleSection: "Uncategorized",
+    url: process.env.NEXTAUTH_URL + "/posts/" + data.slug,
+  };
+
   return (
-    <div className="w-full text-left">
-      <TypographyH1>{data.title}</TypographyH1>
-      <TypographyP>{data.content}</TypographyP>
-      <TypographyMuted>{data.createdAt.toLocaleDateString()}</TypographyMuted>
-    </div>
+    <>
+      <section>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </section>
+      <article className="w-full text-left">
+        <TypographyH1>{data.title}</TypographyH1>
+        <TypographyP>{data.content}</TypographyP>
+        <TypographyMuted>{data.createdAt.toLocaleDateString()}</TypographyMuted>
+      </article>
+    </>
   );
 }
