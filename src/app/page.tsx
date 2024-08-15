5100d49ef8b357bbb95df081b05b4bbec0b535d8
Piotr Kuncy <kuncypiotr@gmail.com>
2024-08-15 10:05:48 +0200

    feat: latest post on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+import Post from "@/components/post/post";
 import { PostList } from "@/components/post/post-list";
 import { Spinner } from "@/components/ui/spinner";
+import { TypographyH2 } from "@/components/ui/typography";
+import { env } from "@/env";
+import { api } from "@/trpc/server";
 import { Suspense } from "react";
 
 export default async function Home() {
+  const post = await api.post.get({ latest: true });
+  const blog = {
+    name: env.BLOG_NAME,
+    language: env.BLOG_LANGUAGE,
+  };
   return (
-    <Suspense fallback={<Spinner />}>
-      <PostList href="/posts" />
-    </Suspense>
+    <>
+      {post && (
+        <>
+          <TypographyH2 className="w-full text-left">Latest post</TypographyH2>
+          <Post data={post} blog={blog} />
+        </>
+      )}
+      <Suspense fallback={<Spinner />}>
+        <PostList href="/posts" />
+      </Suspense>
+    </>
   );
 }
