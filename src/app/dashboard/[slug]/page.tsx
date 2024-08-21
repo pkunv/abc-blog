07e68adf2de0f1ce98473f16fa2eca86ab24a0f6
Piotr Kuncy <piotr.kuncy@neuca.pl>
2024-08-21 15:11:16 +0200

    fix: post form ext bundle by suspense

--- src/app/dashboard/[slug]/page.tsx
+++ src/app/dashboard/[slug]/page.tsx
@@ -1,20 +1,24 @@
-import PostForm from "@/components/post/post-form";
+import { Spinner } from "@/components/ui/spinner";
 import { TypographyH2 } from "@/components/ui/typography";
 import { api } from "@/trpc/server";
 import { notFound } from "next/navigation";
+import { lazy, Suspense } from "react";
 
 export default async function DashboardPostPage({
   params,
 }: {
   params: { slug: string };
 }) {
+  const PostForm = lazy(() => import("../../../components/post/post-form"));
   const post = await api.post.get({ slug: params.slug, placement: "ALL" });
   if (post === null) notFound();
 
   return (
     <>
       <TypographyH2 className="w-full text-left">Update your post</TypographyH2>
-      <PostForm data={post} />
+      <Suspense fallback={<Spinner />}>
+        <PostForm data={post} />
+      </Suspense>
     </>
   );
 }
