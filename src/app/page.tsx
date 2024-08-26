1f3e69205af44af4900bb893e5fcc08bae702361
Piotr Kuncy <piotr.kuncy@neuca.pl>
2024-08-26 15:25:54 +0200

    feat: featured post on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ import { blogProps } from "@/lib/getBlogProps";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
+  const featuredPost = await api.post.get({ placement: "FEATURED" });
   const post = await api.post.get({});
 
   return (
     <>
+      {featuredPost && (
+        <>
+          <TypographyH2 className="w-full text-left">
+            Featured post
+          </TypographyH2>
+          <Post post={featuredPost} blog={blogProps} />
+        </>
+      )}
       {post && (
         <>
           <TypographyH2 className="w-full text-left">Latest post</TypographyH2>
